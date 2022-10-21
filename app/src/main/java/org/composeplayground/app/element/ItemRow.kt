c68c350db89dfd03897895504f340ff9ad1dc0b2
Mate Herber <mate.herber@outlook.com>
2022-10-21 13:00:34 +0200

    Basic animation

--- app/src/main/java/org/composeplayground/app/element/ItemRow.kt
+++ app/src/main/java/org/composeplayground/app/element/ItemRow.kt
@@ -1,6 +1,9 @@
 package org.composeplayground.app.element
 
+import androidx.compose.animation.Animatable
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
@@ -8,24 +11,36 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import kotlin.random.Random
+import kotlinx.coroutines.launch
 import org.composeplayground.app.theme.CombinedPreviews
 import org.composeplayground.app.theme.ComposePlaygroundPreview
 import org.composeplayground.app.theme.spacing
 
 @Composable
 fun ItemRow(modifier: Modifier = Modifier, color: Long, title: String) {
+    val animatedColor = remember {
+        Animatable(Color(color))
+    }
+    val scope = rememberCoroutineScope()
     Row(
-        modifier = modifier,
+        modifier = modifier.clickable {
+            scope.launch {
+                animatedColor.animateTo(Color(Random.nextLong(0xFFFFFFFF)), tween(5000))
+            }
+        },
         horizontalArrangement = Arrangement.spacedBy(MaterialTheme.spacing.large),
         verticalAlignment = Alignment.CenterVertically
     ) {
         Box(
             modifier = Modifier
-                .background(Color(color))
+                .background(animatedColor.value)
                 .size(80.dp)
         )
         Text(
